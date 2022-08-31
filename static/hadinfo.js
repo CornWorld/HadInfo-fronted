38131f3bc02989c1f9d36424a9ced9e5fe477f10
CornWorld <cw@had.ink>
2022-08-31 20:54:14 +0800

    [Feat] Add base64 encode behavior to the content value when submitting

--- static/hadinfo.js
+++ static/hadinfo.js
@@ -1,7 +1,12 @@
 let agreementCheckbox=$("#agreement");
+let addFormSubmitButton=$("#addFormSubmitButton");
 window.onload=function () {
     agreementCheckbox.change(function() {
         $("#submitButton")[0].className="ui button "+(agreementCheckbox[0].checked?"":"disabled");
        
     });
+    addFormSubmitButton.on("click", function() {
+        $("#content").val(window.btoa($("#content_input").val()));
+        $("#addForm").submit();
+    })
 }
